682b25dc10384ed70f87eeddc9d7c4dd66a78e32
Kunio Sawatani <devel@fathens.org>
2017-06-12 20:11:39 +0900

    Enable unicode input in haskell-mode

--- init.d/my-haskell.el
+++ init.d/my-haskell.el
@@ -6,6 +6,7 @@
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
 (add-hook 'haskell-mode-hook 'haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-unicode-input-method)
 ;; (add-hook 'haskell-mode-hook 'view-mode)
 
 (setq haskell-process-type 'stack-ghci)
