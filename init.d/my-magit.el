cbbe8c8d7f46944023708cddcce232690bebcf1b
Kunio Sawatani <devel@fathens.org>
2017-07-17 13:39:40 +0900

    Add Ignore Files Sestion to Magit Mode

--- init.d/my-magit.el
+++ init.d/my-magit.el
@@ -5,4 +5,16 @@
 (require 'magit-gitflow)
 (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
 
+(defun magit-ignored-files ()
+  (magit-git-items
+   "ls-files" "--others" "--ignored" "--exclude-standard" "-z" "--directory"))
+(defun magit-insert-ignored-files ()
+  (-when-let (files (magit-ignored-files))
+    (magit-insert-section (ignored)
+      (magit-insert-heading "Ignored files:")
+      (magit-insert-un/tracked-files-1 files nil)
+      (insert ?\n))))
+(magit-add-section-hook 'magit-status-sections-hook
+                        'magit-insert-ignored-files)
+
 (provide 'my-magit)
