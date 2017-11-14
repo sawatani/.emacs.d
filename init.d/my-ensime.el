b5d28348977137c1a212fecbc35525a4d159afec
Kunio Sawatani <hopstep@sawatani.jp>
2017-11-14 19:04:15 +0900

    Fix my-ensime

--- init.d/my-ensime.el
+++ init.d/my-ensime.el
@@ -1,11 +1,9 @@
 (eval-after-load "ensime-mode"
-  (message "entering ensime-mode")
+  (ensime)
 )
 (defun my-ensime-mode-hook ()
-  (evil-define-key 'insert ensime-mode-map (kbd ".") 'scala/completing-dot)
   (evil-define-key 'normal ensime-mode-map (kbd "g d") 'ensime-edit-definition)
   (evil-define-key 'normal ensime-mode-map (kbd "g b") 'ensime-pop-find-definition-stack)
-  (ensime)
 )
 (add-hook 'scala-mode-hook 'my-ensime-mode-hook)
 
